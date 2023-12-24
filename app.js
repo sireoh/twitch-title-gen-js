c125965a4c8352a408326a9499511f509f15cadb
Sireoh Seo <64703515+sireoh@users.noreply.github.com>
2023-12-24 14:27:45 -0800

    added custom title function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-﻿const emojis = "🥇\n🏧\n🎄\n🏯\n🎎\n👌\n🎅\n🗽\n🦖\n🗼\n🩹\n🎟️\n🚡\n✈️\n⏰\n⚗️\n👽\n👾\n🚑\n🏺\n⚓\n🐜\n🚗\n🥑\n🐤\n🎒\n🥓\n🏸\n🥯\n🥖\n⚖️\n🎈\n🍌\n🪘\n🍒\n🌸\n🌰\n🐔\n🐿️\n🍫\n🎪\n🌆\n🥂\n📬\n🌧️\n🤡\n🍹\n🥥\n☄️\n🧭\n🎊\n😖\n🍚\n🍪\n🍳\n🐄\n🦀\n🖍️\n💳\n🏏\n🐊\n🥐\n⚔️\n🔮\n🥒\n🥤\n🍛\n🍮\n🍖\n🍡\n🍱\n🍹\n🚲\n🌸\n🐡\n💣\n🦴\n🔖\n📚\n🍾\n💐\n🍜\n🎳\n🧠\n🍞\n🥦\n🧹\n🐞\n🚄\n🎯\n🌯\n🚌\n🧈\n🦋\n🌵\n🐫\n📷\n🏕️\n🕯️\n🍬\n🥫\n🛶\n🗃️\n📑\n🎠\n🎏\n🥕\n😺\n🪑\n📈\n🧀\n🍒\n🌸\n🌰\n🐔\n🐿️\n🍋\n💡\n🦎\n🦙\n🦂\n🍇\n🍏\n🥗\n🎸\n🍔\n🐹\n🙉\n🦔\n🚁\n🌿\n🌺\n🦛\n🍯\n🐝\n☕\n🌭\n🌋\n❄️\n🍦\n🎃\n👖\n🕹️\n🦘\n🛴\n👘\n🔪\n🪁\n🥝\n🏷️\n🐞\n💻\n🍃\n🥬\n🍋\n💡\n🦎\n🦙\n🦐\n🦚\n🐄\n😱\n🥙\n🍂\n⛴️\n🎞️\n🔥\n🚒\n🧯\n🐟\n⛳\n🦩\n🔦\n💾\n🃏\n😳\n🛸\n🥠\n🍀\n🍟\n🍤\n🐸\n⛽\n🎲\n🧄\n💎\n🦒\n🥛\n👓\n🧤\n🌟\n🐐\n🦍\n🍇\n🍏\n🍏\n🌧️\n🍣\n💦\n🌮\n👕\n🥡\n🎋\n🍊\n🍵\n☎️\n📺\n⛺\n🧪\n🧵\n🎫\n🐅\n🚽\n🍅\n🧰\n🚜\n🍹\n🐠\n🌍\n🗞️\n🧶\n🥱\n🤪\n🪀";
+const emojis = "🥇\n🏧\n🎄\n🏯\n🎎\n👌\n🎅\n🗽\n🦖\n🗼\n🩹\n🎟️\n🚡\n✈️\n⏰\n⚗️\n👽\n👾\n🚑\n🏺\n⚓\n🐜\n🚗\n🥑\n🐤\n🎒\n🥓\n🏸\n🥯\n🥖\n⚖️\n🎈\n🍌\n🪘\n🍒\n🌸\n🌰\n🐔\n🐿️\n🍫\n🎪\n🌆\n🥂\n📬\n🌧️\n🤡\n🍹\n🥥\n☄️\n🧭\n🎊\n😖\n🍚\n🍪\n🍳\n🐄\n🦀\n🖍️\n💳\n🏏\n🐊\n🥐\n⚔️\n🔮\n🥒\n🥤\n🍛\n🍮\n🍖\n🍡\n🍱\n🍹\n🚲\n🌸\n🐡\n💣\n🦴\n🔖\n📚\n🍾\n💐\n🍜\n🎳\n🧠\n🍞\n🥦\n🧹\n🐞\n🚄\n🎯\n🌯\n🚌\n🧈\n🦋\n🌵\n🐫\n📷\n🏕️\n🕯️\n🍬\n🥫\n🛶\n🗃️\n📑\n🎠\n🎏\n🥕\n😺\n🪑\n📈\n🧀\n🍒\n🌸\n🌰\n🐔\n🐿️\n🍋\n💡\n🦎\n🦙\n🦂\n🍇\n🍏\n🥗\n🎸\n🍔\n🐹\n🙉\n🦔\n🚁\n🌿\n🌺\n🦛\n🍯\n🐝\n☕\n🌭\n🌋\n❄️\n🍦\n🎃\n👖\n🕹️\n🦘\n🛴\n👘\n🔪\n🪁\n🥝\n🏷️\n🐞\n💻\n🍃\n🥬\n🍋\n💡\n🦎\n🦙\n🦐\n🦚\n🐄\n😱\n🥙\n🍂\n⛴️\n🎞️\n🔥\n🚒\n🧯\n🐟\n⛳\n🦩\n🔦\n💾\n🃏\n😳\n🛸\n🥠\n🍀\n🍟\n🍤\n🐸\n⛽\n🎲\n🧄\n💎\n🦒\n🥛\n👓\n🧤\n🌟\n🐐\n🦍\n🍇\n🍏\n🍏\n🌧️\n🍣\n💦\n🌮\n👕\n🥡\n🎋\n🍊\n🍵\n☎️\n📺\n⛺\n🧪\n🧵\n🎫\n🐅\n🚽\n🍅\n🧰\n🚜\n🍹\n🐠\n🌍\n🗞️\n🧶\n🥱\n🤪\n🪀";
 const words = "nomic \nw/ \neoh \nsleepy \nstrim \norang \nco-working \naort \nmorning \npost-nap \ncuties \nscribble \ndrink wawa \nsometimes \nsky \nhapi \ncozy \nsocks \ncome watch me \ndroo \ncomfi \njk im \npee \ncrai \nis a \nveri \nstrim \nwowow \nhehe \nwatch \nnini \ntime \nyour \nyou are a \nchill \nwith me \nalong with \nlob \nhababa \nreveal \nfeet \nnya \nin the \nyay \ndon't forget to \nget ready and \nits true \nmaybe "
 const emojisArray = emojis.split("\n")
 const wordsArray = words.split("\n")
-const micArray = ["[mic on] ", "[mic off] "]
-const isMicOn = false
+const canGenerate = prompt("1: generate, 2: custom")
+
 
 //Display Bottom Text
 //document.getElementById("b_text").innerHTML = emojisArray;
@@ -24,7 +24,17 @@ for (let i = wordsArray.length - 1; i > 0; i--) {
 // Select the first three emojis
 const randomWords = wordsArray.slice(0, 3).join('');
 
-//Mic Random
-var randomMic = micArray[(Math.random() * micArray.length) | 0]
+switch (canGenerate) {
+  case "1":
+    document.getElementById("b_text").innerHTML = "[mic on] " + randomWords + randomEmojis;
+    break;
+  
+  case "2":
+    const customTitle = prompt("Enter custom title:");
+    document.getElementById("b_text").innerHTML = "[mic on] " + customTitle + randomEmojis;
+    break;
 
-document.getElementById("b_text").innerHTML = randomMic + randomWords + randomEmojis;
+  default:
+    // Handle the default case if canGenerate is neither 1 nor 2
+    break;
+}
